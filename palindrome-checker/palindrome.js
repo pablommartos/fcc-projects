f5539f7a6a5bb5b57b28ee3eaed278190eba4928
pablommartos <pablommartosdev@gmail.com>
2024-03-22 19:35:24 +0100

    22.03.24

--- palindrome-checker/palindrome.js
+++ palindrome-checker/palindrome.js
@@ -2,7 +2,7 @@ const checkBtn = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
 
 const checkPalindrome = () => {
-    let inputValue = textInput.value.toLowerCase();
+    let inputValue = textInput.value.toLowerCase().replace(/\s/g, '');
   if (inputValue === "") {
     alert("Please input a value");
   } else if (inputValue.length === 1 && /[A-Z][a-z]/) {
@@ -11,7 +11,8 @@ const checkPalindrome = () => {
     ).innerHTML = `${textInput.value} is a palindrome`;
   } else if (inputValue.length > 1) {
     // Variable to store the reversed value of the input
-    let reverse = inputValue.split("").reverse().join("");
+    let reverse = inputValue.replace(/\s/g, '').split("").reverse().join("");
+    console.log(reverse);
     if (inputValue === reverse) {
       document.getElementById(
         "result"
